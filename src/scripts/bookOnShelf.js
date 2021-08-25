3f944244c9b9881df79215a92e58a6df08fc7591
Keith Parks <keithparks20@gmail.com>
2021-08-25 10:57:52 -0500

    set the fetch and the getters

---
+++ src/scripts/bookOnShelf.js
@@ -0,0 +1,14 @@
+export const bookShelfHtml = () => {
+    return `
+    <h1>Books on Shelf</h1>
+    <div>
+        <h2>Book Shelf</h2>
+    </div>
+
+    <div>
+        <h2>Books</h2>
+    </div>
+
+    <button>Submit button</button>
+    `
+}
--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,3 +1,5 @@
+const mainContainer = document.querySelector("#mainContainer")
+
 const applicationState = {
     books: [],
     booksOnShelf: [],
@@ -7,3 +9,41 @@ const applicationState = {
         bookShelfTwoHtml: false 
     }
 }
+
+export const fetchBooks = () => {
+    return fetch(`http://localhost:8088/books`)
+        .then(response => response.json())
+        .then(
+            (books) => {
+                applicationState.books = books
+            }
+        )
+}
+
+export const getBooks = () => applicationState.books.map(book => ({ ...book }))
+
+
+export const fetchBooksOnShelf = () => {
+    return fetch(`http://localhost:8088/booksOnShelf`)
+        .then(response => response.json())
+        .then(
+            (booksOnShelf) => {
+                applicationState.booksOnShelf = booksOnShelf
+            }
+        )
+}
+
+export const getBooksOnShelf = () => applicationState.booksOnShelf.map(book => ({ ...book }))
+
+
+export const fetchShelves = () => {
+    return fetch(`http://localhost:8088/shelfs`)
+        .then(response => response.json())
+        .then(
+            (shelf) => {
+                applicationState.shelfs = shelf
+            }
+        )
+}
+
+export const getShelves = () => applicationState.shelfs.map(shelf => ({ ...shelf }))
